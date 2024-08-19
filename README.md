2688c7d02c82c6698f4c13464489f7b1d86e3b15
visaenator <visa.seppanen@gmail.com>
2024-08-19 18:03:19 +0300

    Added optimization possibility and poll the distance to the exists to fasten things up

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 
 Maze solver
-
+===========
 Give a maze where white space is walkable terrain, ^ the starting position and E the exit.
 
-Improvement ideas:
--Fast check:
-    We could check all the entrances first, look for the minimun distance aka straight from start to exit, and determine from there should we even try the options with less steps
--Find the optimal route:
-    The algorithm now just finds the first route that satisfies the step limit
-    Maybe just lowering the step limit after we have found a route and then running the algorithm again with that limit? Would be far from optimal solution
--Optimize the code
-    Damn it takes a long time if there is a lot of open space
+Improvement ideas
+=================
+-Optimize
+    The algorithm isn't good nor optimized right now
+    If there is a lot of white space, the algortihm can take a long time
+    Would propably do a flood-fill
+-Finding the optimal route
+    The algorithm now finds a route and then tries to optimize it by giving the algorithm less steps to work with
+    This is very unefficient because we just throw all the earlier computation into trash and always start from the beginning and it might take ridiculous amount of time
+-Passing arguments
+    What do we really need? Most of the data could be derived from the maze map, so should we just always check from that or check once and pass it around
+    Check all values for example in case of const
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 //Possible maze size can be infinite and not even a square, but lets set the limit somewhere in this exercise
 //Because max movement is 200, starting from the middle and going in straight line would get you a 401x401 maze
 #define MAX_SIZE 402
+
+const int step_difficulties[3] = {20, 150, 200};
 
 typedef
     struct{
@@ -20,24 +23,30 @@ coord set_coordinate(int x, int y){
 void read_maze(char **maze, int x, int y, FILE* ptr);
 bool check_size(int *max_x, int *max_y, FILE* ptr);
 void possible_moves(char **maze, coord start, coord* poss_moves, const int max_x, const int max_y);
-int go_through_maze(char **maze, coord location, const int max_x, const int max_y, int *steps);
-void print_maze(char **maze);
+int go_through_maze(char **maze, coord location, const int max_x, const int max_y, int *stepsm, coord* exits_coords, int num_of_exits);
+
+void free_maze_memory(char **maze, int max_y, coord* exits_coords);
+bool can_reach_exit(const coord location, const coord* exits, const int num_of_exits, const int steps);
+void print_maze(char **maze, const int max_x, const int max_y, coord start);
 
 int main(){
 
     char file_name[20];
     printf("Give the name of the maze file: ");
-    scanf("%s", file_name);
+
+    if(fgets(file_name, 20, stdin)){
+        file_name[strcspn(file_name, "\n")] = 0;
+    }
 
     FILE* file_ptr = fopen(file_name, "r");
     if(file_ptr == NULL){
-        printf("That file does not exist :(");
+        printf("File %s does not exist :(", file_name);
         return 1;
     }
     
     int max_x, max_y = 0;
     bool good_size = check_size(&max_x, &max_y, file_ptr);
-
+    
     if(!good_size){
         fclose(file_ptr);
         return 1;
@@ -51,97 +60,106 @@ int main(){
     read_maze(maze, max_x, max_y, file_ptr);
     fclose(file_ptr);
 
-    bool exit_exists = false;
+    int exits = 0;
     coord start = {-1, -1};
     for(int i = 0; i < max_y; ++i){
         for(int j = 0; j < max_x; ++j){
             char c = maze[i][j];
-            printf("%c", c);
             if(c == '^'){
                 start.x = j;
                 start.y = i;
             }
             else if(c == 'E'){
-                exit_exists = true;
+                ++exits;
             }
         }
-        printf("\n");
     }
+
+    coord* exits_coords = (coord*)malloc(exits * sizeof(coord));
 
     if(start.x == -1 || start.y == -1){
         printf("Couldn't find starting position, aborting...");
+        free_maze_memory(maze, max_y, exits_coords);
+        return 0;
     }
-    if(!exit_exists){
+    if(exits <= 0){
         printf("Couldn't find exit, aborting...");
+        free_maze_memory(maze, max_y, exits_coords);
+        return 0;
     }
-
-    //printf("%d, %d", start.x, start.y);
-
-    int step_difficulties[3] = {50, 100, 200};
-    int result = -2;
-    int steps_taken = 0;
-    int difficulty;
-
-    for(difficulty = 0; difficulty < 3; ++difficulty){
-        printf("Trying with %d steps.\n", step_difficulties[difficulty]);
-        steps_taken = step_difficulties[difficulty];
-        result = go_through_maze(maze, start, max_x, max_y, &steps_taken);
-        if(result == 1){
-            break;
-        }
-        printf("Couldn't finish with %d steps...\n", step_difficulties[difficulty]);
-    }
-
-    //Restore the starting position;
-    maze[start.y][start.x] = '^';
-
-    if(result == 1){
-        printf("Solved the maze!\n");
-        printf("Steps taken: %d\n", step_difficulties[difficulty]-steps_taken);
-    }
-    else {
-        printf("Didn't solve the maze... \n");
-    }
-
+    
+    int temp = 0;
     for(int i = 0; i < max_y; ++i){
         for(int j = 0; j < max_x; ++j){
             char c = maze[i][j];
-            printf("%c", c);
+            if(c == 'E'){
+                coord test_coord = {j, i};
+                exits_coords[temp++] = test_coord;
+            }
         }
-        printf("\n");
     }
 
-    for(int i = 0; i < max_y; ++i){
-        free(maze[i]);
+    int result = -2;
+    int steps = 0;
+    int steps_taken = 0;
+
+    for(int i = 0; i < 3; ++i){
+        if(!can_reach_exit(start, exits_coords, exits, step_difficulties[i])){
+            printf("No exit within reach with %d steps...\n", step_difficulties[i]);
+            continue;
+        }
+
+        printf("Trying with %d steps.\n", step_difficulties[i]);
+        steps = step_difficulties[i];
+        result = go_through_maze(maze, start, max_x, max_y, &steps, exits_coords, exits);
+        if(result == 1){
+            printf("Solved the maze!\n");
+            steps_taken = step_difficulties[i]-steps;
+            printf("Steps taken: %d\n", steps_taken);
+            print_maze(maze, max_x, max_y, start);
+            break;
+        }
+
+        printf("Couldn't finish with %d steps...\n", step_difficulties[i]);
     }
-    free(maze);
+
+    char optimization = 'N';
+    printf("Do you want to try optimization (Y/N)? WARNING! It might take a looooong time: ");
+    while((optimization = getchar()) == '\n' || optimization == EOF) {}
+
+    if(optimization == 'Y' || optimization == 'y'){
+        char** best_so_far = (char**)malloc(max_y * sizeof(char*));
+        for(int i = 0; i < max_y; ++i){
+            best_so_far[i] = (char*)malloc(max_x * sizeof(char));
+        }
+
+        while(true){
+            //Have to empty the old solution for the optimization
+            for(int i = 0; i < max_y; ++i){
+                for(int j = 0; j < max_x; ++j){
+                    best_so_far[i][j] = maze[i][j];
+                    if(maze[i][j] == 'o'){
+                        maze[i][j] = ' ';
+                    }
+                }
+            }
+            --steps_taken;
+            int new_try = steps_taken;
+            printf("Optimizing...\n");
+            result = go_through_maze(maze, start, max_x, max_y, &new_try, exits_coords, exits);
+            if(result != 1){
+                printf("Found solution with %d steps\n", steps_taken+1);
+                break;
+            }
+        }
+
+        print_maze(best_so_far, max_x, max_y, start);
+        free(best_so_far);
+    }
+    
+    free_maze_memory(maze, max_y, exits_coords);
     return 0;
 }
-/*
-void read_maze(char maze[MAX_SIZE][MAX_SIZE], FILE* ptr){
-    char ch;
-    int x, y = 0;
-    int read_amount = 0;
-    int max_x, max_y = 0;
-
-    do{
-        ch = fgetc(ptr);
-        if(ch == '\n'){
-            maze[y][x] = 'X';
-            ++y;
-            x = 0;
-        }
-        else{
-            maze[y][x] = ch;
-            ++x;
-        }
-        ++read_amount;
-    }
-    while(ch != EOF);
-    maze[y][x] = 'Z';
-}
-
-*/
 
 void read_maze(char **maze, int x, int y, FILE* ptr){
     char ch;
@@ -194,7 +212,7 @@ bool check_size(int *max_x, int *max_y, FILE* ptr){
     return true;
 }
 
-int go_through_maze(char **maze, coord location, const int max_x, const int max_y, int *steps){
+int go_through_maze(char **maze, coord location, const int max_x, const int max_y, int *steps, coord* exits_coords, int num_of_exits){
 
     //0 = Continue
     //1 = Found the exit
@@ -209,21 +227,19 @@ int go_through_maze(char **maze, coord location, const int max_x, const int max_
     }
 
     //We have ran out of steps
-    if(*steps <= 0){
+    if(!can_reach_exit(location, exits_coords, num_of_exits, *steps) || *steps <= 0){
         return -2;
     }
 
-    maze[location.y][location.x] = 'X';
+    maze[location.y][location.x] = 'o';
     --(*steps);
     
     coord poss_moves[4];
     possible_moves(maze, location, poss_moves, max_x, max_y);
 
     for(int i = 0; i < 4; ++i){
-        //printf("X:%d Y:%d Steps:%d\n", poss_moves[i].x, poss_moves[i].y, *steps);
-
         if(poss_moves[i].x != -1){
-            solution = go_through_maze(maze, poss_moves[i], max_x, max_y, steps);
+            solution = go_through_maze(maze, poss_moves[i], max_x, max_y, steps, exits_coords, num_of_exits);
             if(solution == 1){
                 return solution;
             }
@@ -233,6 +249,7 @@ int go_through_maze(char **maze, coord location, const int max_x, const int max_
         }
     }
 
+    //We have gotten into a dead end, back up
     maze[location.y][location.x] = ' ';
     ++(*steps);
     return -1;
@@ -267,4 +284,34 @@ void possible_moves(char **maze, coord start, coord* poss_moves, const int max_x
     }
 
 
+}
+
+void free_maze_memory(char **maze, int max_y, coord* exits_coords){
+    for(int i = 0; i < max_y; ++i){
+        free(maze[i]);
+    }
+    free(maze);
+    free(exits_coords);
+}
+
+bool can_reach_exit(const coord location, const coord* exits, const int num_of_exits, const int steps){
+    for(size_t i = 0; i < num_of_exits; ++i){
+        //We have atleast one exit within reach
+        if((abs(location.x - exits[i].x) + abs(location.y - exits[i].y)) < steps){
+            return true;
+        }
+    }
+    //All exits are too far away
+    return false;
+}
+
+void print_maze(char **maze, const int max_x, const int max_y, coord start){
+    //Restore the starting position
+    maze[start.y][start.x] = '^';
+    for(int i = 0; i < max_y; ++i){
+        for(int j = 0; j < max_x; ++j){
+            printf("%c", maze[i][j]);
+        }
+        printf("\n");
+    }
 }
